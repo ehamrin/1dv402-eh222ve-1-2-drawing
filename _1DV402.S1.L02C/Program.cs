536eb4775e8bc00f7caee438a2198e119fb72165
eh222ve <eh222ve@student.lnu.se>
2014-09-09 14:47:37 +0200

    Added comments above functions

--- _1DV402.S1.L02C/Program.cs
+++ _1DV402.S1.L02C/Program.cs
@@ -40,9 +40,13 @@ namespace _1DV402.S1.L02C
             } while (IsContinuing());
         }
 
+        /* ReadOddByte() skriver ut en medskickad prompt och läser
+         * av värdet som kommer in. Därefter valideras detta mot
+         * NaN och om det är udda och större än 0. Om det inte stämmer
+         * skickas felmeddelande och loopen startas om i funktionen.
+         */
         static byte ReadOddByte(string prompt, byte maxValue = 255)
         {
-
             byte maxCount = 0;
 
             do
@@ -82,6 +86,10 @@ namespace _1DV402.S1.L02C
             return maxCount;
         }
 
+        /*RenderDiamond() har 2st loopar, den första tar hand om övre 
+         * halvan inklusive "bältet". Den andra loopen tar han om undre 
+         * halvan och subtraherar två för att undvika dubbel utskrift av
+         * "bältet". */
         static void RenderDiamond(byte maxCount)
         {
             //Top triangle and widest point.
@@ -97,6 +105,9 @@ namespace _1DV402.S1.L02C
             }
         }
 
+        /*RenderRow() Räknar först ut hur många mellanslag som bör
+         * vara på raden för att centrera diamanten. Därefter skrivs
+         * mellanslag och asterisker ut */
         static void RenderRow(int maxCount, int asteriskCount)
         {
             //Determine number of spaces needed on row
@@ -116,6 +127,10 @@ namespace _1DV402.S1.L02C
             Console.WriteLine();
         }
 
+        /* IsContinuing() skriver först ut ett meddelande till användaren
+         * och lyssnar där efter Escape-tangenten, annars tar den bort
+         * allt som finns i konsol-fönstret och låter loopen fortsätta med
+         * värdet true */
         static bool IsContinuing()
         {
             ConsoleKeyInfo keyPress;
@@ -136,6 +151,10 @@ namespace _1DV402.S1.L02C
             }
         }
 
+        /* ViewMessage() kollar först om det är ett felmeddelande som
+         * ska skrivas ut och sätter färger därefter. Sedan kollar den
+         * om det finns en referens till det meddelandet som ska skickas
+         * med och skriver ut meddelandet.*/
         static void ViewMessage(string message, bool isError = false, string referenceString = null)
         {
             if (isError)
